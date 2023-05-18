a6fba02203068f052fa5270c4327501250831ea6
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2023-05-18 16:30:44 -0300

    side navigaation, drop menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
 
 function openNav() {
     document.getElementById('navLinks').style.width = '240px'
-    document.getElementById('app').style.marginRight = '240px'
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.75)";
+}
+document.getElementById('openMenu').addEventListener('click', () => {
+    openNav()
+})
+
+function closeNav() {
+    document.getElementById('navLinks').style.width = '0'
+    document.body.style.backgroundColor = "#fff";
+}
+document.getElementById('closeMenu').addEventListener('click', () => {
+    closeNav()
+})
+
+function openDropdown() {
+    document.querySelector('.navbar-dropdown-menu').classList.add('show')
 }
 
+document.getElementById('features').addEventListener('click', () => {
+    if (document.querySelector('.navbar-menu__features').classList.contains('hide-menu')) {
+        document.querySelector('.navbar-menu__features').classList.add('show-menu')
+        document.querySelector('.navbar-menu__features').classList.remove('hide-menu')
+    } else {
+        document.querySelector('.navbar-menu__features').classList.add('hide-menu')
+        document.querySelector('.navbar-menu__features').classList.remove('show-menu')
+    }
+})
 
-
-document.getElementById('menuBtn').addEventListener('click', () => {
-    openNav()
+document.getElementById('company').addEventListener('click', () => {
+    if (document.querySelector('.navbar-menu__company').classList.contains('hide-menu')) {
+        document.querySelector('.navbar-menu__company').classList.add('show-menu')
+        document.querySelector('.navbar-menu__company').classList.remove('hide-menu')
+    } else {
+        document.querySelector('.navbar-menu__company').classList.add('hide-menu')
+        document.querySelector('.navbar-menu__company').classList.remove('show-menu')
+    }
 })
