0e8d7452f4f55ec394fe1826fbcb35258371577a
claudio-dev1993 <claudinhoroberto2008@gmail.com>
2023-05-21 20:29:03 -0300

    Layout for tablet display

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
 
+function switchArrow() {
+   const options = document.querySelectorAll('.navbar-options__button')
+   options.forEach( op => {
+    if (op.classList.contains('arrow-down')) {
+        op.classList.replace('arrow-down', 'arrow-up')
+   } else {
+        op.classList.replace('arrow-up', 'arrow-down')
+   }
+   })
+}
 function openNav() {
-    document.getElementById('navLinks').style.width = '240px'
+    if (window.screen.width.valueOf() < 768) {
+        document.getElementById('navLinks').style.width = '240px'
+    } else {
+        document.getElementById('navLinks').style.width = '480px'
+    }
     document.querySelector('.navbar__collapse').style.width = '100%'
     document.querySelector('.navbar__collapse').style.backgroundColor = "rgba(0,0,0,0.75)";
 }
-document.getElementById('openMenu').addEventListener('click', () => {
-    openNav()
-})
-
 function closeNav() {
     document.getElementById('navLinks').style.width = '0'
     document.querySelector('.navbar__collapse').style.width = '0'
 }
+
+document.getElementById('openMenu').addEventListener('click', () => {
+    openNav()
+})
+
 document.getElementById('closeMenu').addEventListener('click', () => {
     closeNav()
 })
@@ -19,21 +34,19 @@ document.getElementById('closeMenu').addEventListener('click', () => {
 document.getElementById('features').addEventListener('click', () => {
     const menuFeatures = document.querySelector('.navbar-dropdown-menu__features')
     if (menuFeatures.classList.contains('hide-menu')) {
-        menuFeatures.classList.add('show-menu')
-        menuFeatures.classList.remove('hide-menu')
+        menuFeatures.classList.replace('hide-menu','show-menu')
     } else {
-        menuFeatures.classList.add('hide-menu')
-        menuFeatures.classList.remove('show-menu')
+        menuFeatures.classList.replace('show-menu', 'hide-menu')
     }
+    switchArrow()
 })
 
 document.getElementById('company').addEventListener('click', () => {
     const menuCompany = document.querySelector('.navbar-dropdown-menu__company')
     if (menuCompany.classList.contains('hide-menu')) {
-        menuCompany.classList.add('show-menu')
-        menuCompany.classList.remove('hide-menu')
+        menuCompany.classList.replace('hide-menu','show-menu')
     } else {
-        menuCompany.classList.add('hide-menu')
-        menuCompany.classList.remove('show-menu')
+        menuCompany.classList.replace('show-menu', 'hide-menu')
     }
+    switchArrow()
 })
